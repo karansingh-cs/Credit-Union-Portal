5ad513dad262aef1fe028cdcbfba4e53db23375b
Karan <66702261+karansingh-cs@users.noreply.github.com>
2026-03-09 00:36:03 -0500

    Added the EF Core basics
    Added the models
    Added the DbContext
    Added to program.cs
    Added connection string

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CreditUnionPortal.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CreditUnionPortal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public  AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Member> Members { get; set; } //creates table called "Members"
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+    }
+}

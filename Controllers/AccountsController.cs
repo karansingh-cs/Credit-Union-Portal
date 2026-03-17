d71f797e0234cc6e9478a2c86ea0df0375b50109
Karan <karanbir97@outlook.com>
2026-03-17 17:11:58 -0500

    Made some changes in pc env

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CreditUnionPortal.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CreditUnionPortal.Controllers
 {
     public class AccountsController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
+        private readonly AppDbContext _context;
+
+        public AccountsController(AppDbContext context)
+        { 
+            _context = context;
         }
+
+        public IActionResult Details(int id)
+        {
+            var account = _context.Accounts // Go to the Accounts table
+                .Include(a => a.Transactions) // Also bring along each account's transactions
+                .FirstOrDefault(a => a.AccountId == id); // From all those accounts, find the first one where the AccountId matches the id I was given
+            if (account == null)
+                return NotFound();
+            
+            return View(account);
+        }
+
     }
 }
